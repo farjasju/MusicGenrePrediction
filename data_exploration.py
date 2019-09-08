a2635780ad88c933e9d9406e7f93b14611c5b1b8
Luis Fernando Lins <luisfernandolins@Luiss-MacBook-Pro.local>
2019-09-08 13:10:15 -0300

    Getting the correlation matrix

--- data_exploration.py
+++ data_exploration.py
@@ -7,3 +7,11 @@ data = pd.read_csv('data/data.csv')
 print(data.columns)
 print(data.cov())
 print(data.describe())
+
+//Geting the correlation matrix
+f = plt.figure(figsize=(19, 15))
+plt.matshow(df.corr(), fignum=f.number)
+plt.xticks(range(df.shape[1]), df.columns, fontsize=10, rotation=90)
+plt.yticks(range(df.shape[1]), df.columns, fontsize=10)
+cb = plt.colorbar()
+cb.ax.tick_params(labelsize=12)
