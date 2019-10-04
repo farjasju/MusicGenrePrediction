1a32c00cb4b9b4862bfb4c6dd37e4e3628ac224c
Jules Farjas <julesfarjas@gmail.com>
2019-10-04 11:45:48 -0300

    Workin on it

--- learning.py
+++ learning.py
@@ -6,45 +6,67 @@ from sklearn import model_selection
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 
-data = pd.read_csv("./data/data.csv")
-print(data.head())
+import matplotlib.pyplot as plt
+import seaborn as sn
 
-# Data matrix
-X = data.to_numpy()[:, 1:-1]
+def load_data():
+    data = pd.read_csv("./data/data.csv")
+    print(data.head())
 
-# Label matrix
-y = data.to_numpy()[:, -1]
-y = y.flatten()
+    # Data matrix
+    X = data.to_numpy()[:, 1:-1]
 
-# transformer en un problème de classification binaire
-lb = preprocessing.LabelBinarizer()
-lb.fit(y)
-print(lb.classes_)
-y_binarized = lb.transform(y)
+    # Label matrix
+    y = data.to_numpy()[:, -1]
+    y = y.flatten()
 
-# Creating the Training and Test set from data
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y_binarized, test_size = 0.25, random_state = 21)
+    # Transform the problem in a binary classification problem
+    lb = preprocessing.LabelBinarizer()
+    lb.fit(y)
+    print(lb.classes_)
+    y_binarized = lb.transform(y)
 
-# Feature Scaling
-scaler = preprocessing.StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+    # Creating the Training and Test set from data
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y_binarized, test_size = 0.25, random_state = 21)
 
-# Fitting Random Forest Classification to the Training set
-classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 42)
-classifier.fit(X_train, y_train)
+    # Feature Scaling
+    scaler = preprocessing.StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    return X_train, X_test, y_train, y_test, lb
 
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
+def evaluate(y_pred, y_test, lb):
+    print("Average precision score:", str(metrics.average_precision_score(y_pred, y_test)))
+    print("Accuracy score:", str(metrics.accuracy_score(y_pred, y_test)))
+    print("F1 score:", str(metrics.f1_score(y_pred, y_test, average='micro')))
+    print("Precision score:", str(metrics.precision_score(y_pred, y_test, average='micro')))
+    print("Recall score:", str(metrics.recall_score(y_pred, y_test, average='micro')))
+    print("ROC AUC score:", str(metrics.roc_auc_score(y_pred, y_test, average='micro')))
+    
+    # ROC Curve ?
 
-y_test_labels = lb.inverse_transform(y_test)
-y_pred_labels = lb.inverse_transform(y_pred)
+    cm = metrics.confusion_matrix(y_pred.argmax(1), y_test.argmax(1))
+    df_cm = pd.DataFrame(cm, index = [i for i in lb.classes_],
+                  columns = [i for i in lb.classes_])
+    plt.figure(figsize = (10,7))
+    ax = sn.heatmap(df_cm, annot=True)
+    ax.set_ylim(10.0, 0)
+    plt.show()
 
-for x in range(100):
-    print("ORIGINAL:", y_test_labels[x], "PREDICTED:", y_pred_labels[x])
+def main():
+    # Loading the dataset
+    X_train, X_test, y_train, y_test, lb = load_data()
 
-print(metrics.average_precision_score(y_pred, y_test))
-print(metrics.accuracy_score(y_pred, y_test))
-print(metrics.f1_score(y_pred, y_test, average='micro'))
-print(metrics.precision_score(y_pred, y_test, average='micro'))
-print(metrics.recall_score(y_pred, y_test, average='micro'))
+    # Fitting Random Forest Classification to the Training set
+    classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 42)
+    classifier.fit(X_train, y_train)
+
+    # Predicting the Test set results
+    y_pred = classifier.predict(X_test)
+    y_test_labels = lb.inverse_transform(y_test)
+    y_pred_labels = lb.inverse_transform(y_pred)
+
+    evaluate(y_pred, y_test, lb)
+
+if __name__ == '__main__':
+    main()
