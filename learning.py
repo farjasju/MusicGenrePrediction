e832b04410d62172cccc11b0941280bcf11987ee
Jules Farjas <julesfarjas@gmail.com>
2019-10-05 11:58:19 -0300

    Removing label binarization

--- learning.py
+++ learning.py
@@ -21,41 +21,41 @@ def load_data():
     y = y.flatten()
 
     # Transform the problem in a binary classification problem
-    lb = preprocessing.LabelBinarizer()
-    lb.fit(y)
-    print(lb.classes_)
-    y_binarized = lb.transform(y)
+    # lb = preprocessing.LabelBinarizer()
+    # lb.fit(y)
+    # print(lb.classes_)
+    # y_binarized = lb.transform(y)
 
     # Creating the Training and Test set from data
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y_binarized, test_size = 0.25, random_state = 21)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size = 0.25, random_state = 21)
 
     # Feature Scaling
     scaler = preprocessing.StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
-    return X_train, X_test, y_train, y_test, lb
+    return X_train, X_test, y_train, y_test
 
-def evaluate(y_pred, y_test, lb):
-    print("Average precision score:", str(metrics.average_precision_score(y_pred, y_test)))
-    print("Accuracy score:", str(metrics.accuracy_score(y_pred, y_test)))
-    print("F1 score:", str(metrics.f1_score(y_pred, y_test, average='micro')))
-    print("Precision score:", str(metrics.precision_score(y_pred, y_test, average='micro')))
-    print("Recall score:", str(metrics.recall_score(y_pred, y_test, average='micro')))
-    print("ROC AUC score:", str(metrics.roc_auc_score(y_pred, y_test, average='micro')))
+def evaluate(y_pred, y_test):
+    # print("Average precision score:", str(metrics.average_precision_score(y_pred, y_test)))
+    print("Accuracy score:", str(metrics.accuracy_score(y_test, y_pred)))
+    print("F1 score:", str(metrics.f1_score(y_test, y_pred, average='micro')))
+    print("Precision score:", str(metrics.precision_score(y_test, y_pred, average='micro')))
+    print("Recall score:", str(metrics.recall_score(y_test, y_pred, average='micro')))
+    # print("ROC AUC score:", str(metrics.roc_auc_score(y_pred, y_test, average='micro')))
     
     # ROC Curve ?
 
-    cm = metrics.confusion_matrix(y_pred.argmax(1), y_test.argmax(1))
-    df_cm = pd.DataFrame(cm, index = [i for i in lb.classes_],
-                  columns = [i for i in lb.classes_])
-    plt.figure(figsize = (10,7))
-    ax = sn.heatmap(df_cm, annot=True)
-    ax.set_ylim(10.0, 0)
-    plt.show()
+    # cm = metrics.confusion_matrix(y_pred.argmax(1), y_test.argmax(1))
+    # df_cm = pd.DataFrame(cm, index = [i for i in lb.classes_],
+    #               columns = [i for i in lb.classes_])
+    # plt.figure(figsize = (10,7))
+    # ax = sn.heatmap(df_cm, annot=True)
+    # ax.set_ylim(10.0, 0)
+    # plt.show()
 
 def main():
     # Loading the dataset
-    X_train, X_test, y_train, y_test, lb = load_data()
+    X_train, X_test, y_train, y_test = load_data()
 
     # Fitting Random Forest Classification to the Training set
     classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 42)
@@ -63,10 +63,10 @@ def main():
 
     # Predicting the Test set results
     y_pred = classifier.predict(X_test)
-    y_test_labels = lb.inverse_transform(y_test)
-    y_pred_labels = lb.inverse_transform(y_pred)
+    # y_test_labels = lb.inverse_transform(y_test)
+    # y_pred_labels = lb.inverse_transform(y_pred)
 
-    evaluate(y_pred, y_test, lb)
+    evaluate(y_pred, y_test)
 
 if __name__ == '__main__':
     main()
