6890ac86340c911571903fdebbd145ef7098f1ae
Jules <julesfarjas@gmail.com>
2019-12-07 10:11:14 -0300

    Bit of formatting

--- data_exploration.py
+++ data_exploration.py
@@ -29,11 +29,13 @@ from scipy.spatial.distance import pdist, squareform
 
 import seaborn as sns
 
+
 def pca(x, y, num_comp):
     pca = PCA(n_components=num_comp)
     projected = pca.fit_transform(x, y)
     print(pca.explained_variance_ratio_)
     return projected
+
 
 def correlation_matrix(data, filename='output'):
     f = plt.figure(figsize=(19, 15))
@@ -42,16 +44,19 @@ def correlation_matrix(data, filename='output'):
     plt.yticks(range(data.shape[1]), data.columns, fontsize=10)
     cb = plt.colorbar()
     cb.ax.tick_params(labelsize=12)
-    plt.savefig(os.path.join('results', 'correlation matrix ' + filename + '_distribution.png'))
+    plt.savefig(os.path.join('results', 'correlation matrix ' +
+                             filename + '_distribution.png'))
+
 
 def distribution_plot(data, filename='output'):
-    #sns.set(color_codes=True)
+    # sns.set(color_codes=True)
     plot = sns.distplot(data, rug=False, hist=True)
     fig = plot.get_figure()
-    fig.savefig(os.path.join('results',filename + '_distribution.png'))
+    fig.savefig(os.path.join('results', filename + '_distribution.png'))
     plt.clf()
 
-def LDAwithGraphics(x,y,data):
+
+def LDAwithGraphics(x, y, data):
 
     lda = LDA(n_components=num_comp)
     X_lda = lda.fit_transform(x, y)
@@ -59,22 +64,22 @@ def LDAwithGraphics(x,y,data):
     for i in range(0, num_comp):
         for j in range(0, num_comp):
             if (i != j):
-                plt.figure(figsize=(10,8))
-                for lab, col in zip(('blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock'), ['red','green','blue','purple','gray','brown','orange','cyan','pink','olive']):
+                plt.figure(figsize=(10, 8))
+                for lab, col in zip(('blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock'), ['red', 'green', 'blue', 'purple', 'gray', 'brown', 'orange', 'cyan', 'pink', 'olive']):
                     plt.scatter(
-                        X_lda[y == lab,i],
-                        X_lda[y == lab,j],
+                        X_lda[y == lab, i],
+                        X_lda[y == lab, j],
                         c=col,
                         label=lab,
                         alpha=0.7
-                        )
+                    )
                 plt.xlabel('component'+str(i))
                 plt.ylabel('component'+str(j))
                 plt.title('LDA')
                 plt.legend()
-                plt.savefig(os.path.join('results', 'projection_matrix_'+str(i)+'_'+str(j)+'.png'))
+                plt.savefig(os.path.join(
+                    'results', 'projection_matrix_'+str(i)+'_'+str(j)+'.png'))
                 plt.clf()
-
 
     # plt.figure(figsize=(10,8))
     # for lab, col in zip(('blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock'), ['red','green','blue','purple','gray','brown','orange','cyan','pink','olive']):
@@ -121,22 +126,25 @@ def LDAwithGraphics(x,y,data):
 
     return lda
 
+
 def scatter_plot(data):
     pd.plotting.scatter_matrix(data, diagonal="kde")
     plt.tight_layout()
     plt.show()
 
+
 def plot_confusion_matrix(matrix, show=True, save=False, filename=None, title='', subtitle=''):
     labels = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop',
- 'reggae', 'rock']
-    ax = sns.heatmap(matrix, annot=True, xticklabels=labels, yticklabels=labels)
-    ax.set_ylim(10.0, 0)
+              'reggae', 'rock']
+    ax = sns.heatmap(matrix, annot=True,
+                     xticklabels=labels, yticklabels=labels)
+    ax.set_ylim(100.0, 0)
     plt.suptitle(title, fontsize=9)
     plt.title(subtitle, {'fontsize': 9})
     plt.xlabel('Predicted')
     plt.ylabel('True')
-    ax.set_yticklabels(ax.get_yticklabels(), rotation =0)
-    ax.set_xticklabels(ax.get_yticklabels(), rotation =90)
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
+    ax.set_xticklabels(ax.get_yticklabels(), rotation=90)
     plt.margins(0.2)
 
     # ax.set_xticklabels(['',''] + labels, {'fontsize': 8,
@@ -152,45 +160,52 @@ def plot_confusion_matrix(matrix, show=True, save=False, filename=None, title=''
             raise Exception('Please specify a filename for the plot image')
         else:
             fig = ax.get_figure()
-            fig.savefig(os.path.join('results',filename + '_cm.png'))
+            fig.savefig(os.path.join('results', filename + '_cm.png'))
     plt.clf()
+
 
 def run_classifier(classifier, X_train, X_test, y_train, y_test):
     y_pred = classifier.fit(X_train, y_train).predict(X_test)
     cm = confusion_matrix(y_test, y_pred)
     return classifier, y_pred, cm
 
+
 def title_generator(items):
     for i in range(len(items)):
         items[i] = str(items[i])
     return ' · '.join(items)
 
+
 def subtitle_generator(y_test, y_pred):
-    acc = str(round(accuracy_score(y_test, y_pred),2))
-    prec = str(round(metrics.precision_score(y_test, y_pred, average='macro'),2))
-    rec = str(round(metrics.recall_score(y_test, y_pred, average='macro'),2))
-    f1 = str(round(metrics.f1_score(y_test, y_pred, average='macro'),2))
+    acc = str(round(accuracy_score(y_test, y_pred), 2))
+    prec = str(round(metrics.precision_score(
+        y_test, y_pred, average='macro'), 2))
+    rec = str(round(metrics.recall_score(y_test, y_pred, average='macro'), 2))
+    f1 = str(round(metrics.f1_score(y_test, y_pred, average='macro'), 2))
     # print("ROC AUC score:", str(metrics.roc_auc_score(y_pred, y_test, average='macro')))
     subtitle = ["acc=" + acc, "prec=" + prec, "rec=" + rec, "f1=" + f1]
     return title_generator(subtitle)
+
 
 def cross_validation(clf, X, y):
     scores = cross_val_score(clf, X, y, cv=5, scoring='f1_macro')
     return scores.mean()
 
+
 def main():
     data = pd.read_csv("./data/data.csv")
-    data.drop(['filename'],axis=1, inplace=True)
-    
-    X = data.iloc[:,1:-1].values
-    y = data.iloc[:,-1].values
+    data.drop(['filename'], axis=1, inplace=True)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.1, random_state=0)
-    
+    X = data.iloc[:, 1:-1].values
+    y = data.iloc[:, -1].values
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, stratify=y, test_size=0.5, random_state=0)
+
     sc = StandardScaler()
     X_train = sc.fit_transform(X_train)
     X_test = sc.transform(X_test)
-    
+
     lda = LDA()
     X_train = lda.fit_transform(X_train, y_train)
     X_test = lda.transform(X_test)
@@ -199,21 +214,20 @@ def main():
     X_lda = lda.fit_transform(X_scaled, y)
 
     # scatter_plot(pd.DataFrame(X_train)) ##scatterplot to see the LDA
-    
+
     classifiers = []
 
-    ## classifier 1 - random forest
+    # classifier 1 - random forest
 
     classifier = RandomForestClassifier(n_estimators=100, random_state=0)
     title = title_generator(["Random Forest", "100 estimators"])
-    classifiers.append({'title': title, 'model':classifier})
+    classifiers.append({'title': title, 'model': classifier})
     # model, y_pred, cm = run_classifier(classifier, X_train, X_test, y_train, y_test)
     # subtitle = subtitle_generator(y_test, y_pred)
     # plot_confusion_matrix(cm, show=False, save=True, filename='random_forest', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
-    
 
-    ## classifier 2 - naive bayes
+    # classifier 2 - naive bayes
 
     classifier = GaussianNB()
     title = title_generator(["Naive Bayes"])
@@ -223,7 +237,7 @@ def main():
     # plot_confusion_matrix(cm, show=False, save=True, filename='naive_bayes', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
 
-    ## classifier 3 - logistic regression
+    # classifier 3 - logistic regression
 
     classifier = LogisticRegression(solver='sag', multi_class='auto')
     title = title_generator(["Logistic Regression", "Solver:'sag'"])
@@ -233,7 +247,7 @@ def main():
     # plot_confusion_matrix(cm, show=False, save=True, filename='logistic_regression', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
 
-    ## classifier 4 - linear SVM
+    # classifier 4 - linear SVM
 
     classifier = SVC(kernel='linear')
     title = title_generator(["Linear SVM"])
@@ -243,27 +257,29 @@ def main():
     # plot_confusion_matrix(cm, show=False, save=True, filename='linear_svm', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
 
-    ## classifier 5 - SVC poly kernel degree 2
+    # classifier 5 - SVC poly kernel degree 2
 
     classifier = SVC(kernel='poly', degree=2, gamma="scale")
-    title = title_generator(["SVC", "kernel='poly'", "degree=2", "gamma='scale'"])
+    title = title_generator(
+        ["SVC", "kernel='poly'", "degree=2", "gamma='scale'"])
     classifiers.append({'title': title, 'model': classifier})
     # model, y_pred, cm = run_classifier(classifier, X_train, X_test, y_train, y_test)
     # subtitle = subtitle_generator(y_test, y_pred)
     # plot_confusion_matrix(cm, show=False, save=True, filename='svc_poly_2', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
 
-    ## classifier 6 - SVC poly kernel degree 4
+    # classifier 6 - SVC poly kernel degree 4
 
     classifier = SVC(kernel='poly', degree=4, gamma="scale")
-    title = title_generator(["SVC", "kernel='poly'", "degree=4", "gamma='scale'"])
+    title = title_generator(
+        ["SVC", "kernel='poly'", "degree=4", "gamma='scale'"])
     classifiers.append({'title': title, 'model': classifier})
     # model, y_pred, cm = run_classifier(classifier, X_train, X_test, y_train, y_test)
     # subtitle = subtitle_generator(y_test, y_pred)
     # plot_confusion_matrix(cm, show=False, save=True, filename='svc_poly_4', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
 
-    ## classifier 7 - SVC gaussian kernel (best so far)
+    # classifier 7 - SVC gaussian kernel (best so far)
 
     classifier = SVC(kernel='rbf', gamma="scale", random_state=0)
     title = title_generator(["SVC", "kernel='rbf'", "gamma='scale'", "C=1"])
@@ -273,7 +289,13 @@ def main():
     # plot_confusion_matrix(cm, show=False, save=True, filename='svc_rbf', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
 
-    ## classifier 8 - SVC sigmoid kernel
+    # classifier 7 bis - SVC gaussian kernel (best so far) - tweaked parameters
+
+    classifier = SVC(kernel='rbf', gamma=0.0204, random_state=0, C=0.9)
+    title = title_generator(["SVC", "kernel='rbf'", "gamma=0.0204", "C=0.9"])
+    classifiers.append({'title': title, 'model': classifier})
+
+    # classifier 8 - SVC sigmoid kernel
 
     classifier = SVC(kernel='sigmoid', gamma="scale")
     title = title_generator(["SVC", "kernel='sigmoid'", "gamma='scale'"])
@@ -283,7 +305,7 @@ def main():
     # plot_confusion_matrix(cm, show=False, save=True, filename='svc_sigmoid', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
 
-    ## classifier 9 - Decision tree
+    # classifier 9 - Decision tree
 
     classifier = tree.DecisionTreeClassifier()
     title = title_generator(["Decision Tree"])
@@ -293,7 +315,7 @@ def main():
     # plot_confusion_matrix(cm, show=False, save=True, filename='decision_tree', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
 
-    ## classifier 10 - KNN 5 neighbors
+    # classifier 10 - KNN 5 neighbors
 
     classifier = KNeighborsClassifier(n_neighbors=5)
     title = title_generator(["Knn", "5 neighbors"])
@@ -303,7 +325,7 @@ def main():
     # plot_confusion_matrix(cm, show=False, save=True, filename='knn_5', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
 
-    ## classifier 11 - KNN 7 neighbors
+    # classifier 11 - KNN 7 neighbors
 
     classifier = KNeighborsClassifier(n_neighbors=7)
     title = title_generator(["Knn", "7 neighbors"])
@@ -313,7 +335,7 @@ def main():
     # plot_confusion_matrix(cm, show=False, save=True, filename='knn_7', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
 
-    ## classifier 12 - KNN 9 neighbors
+    # classifier 12 - KNN 9 neighbors
 
     classifier = KNeighborsClassifier(n_neighbors=9)
     title = title_generator(["Knn", "9 neighbors"])
@@ -323,43 +345,47 @@ def main():
     # plot_confusion_matrix(cm, show=False, save=True, filename='knn_9', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
 
-    ## classifier 13 - Gradient Boosting
+    # classifier 13 - Gradient Boosting
 
-    classifier = GradientBoostingClassifier(n_estimators=100, learning_rate=0.75, max_features=9, max_depth=3, random_state=0)
-    title = title_generator(["Gradient boosting", "n_estimators=100", "learning_rate=0.75", "max_features=9", "max_depth=3"])
+    classifier = GradientBoostingClassifier(
+        n_estimators=100, learning_rate=0.75, max_features=9, max_depth=3, random_state=0)
+    title = title_generator(["Gradient boosting", "n_estimators=100",
+                             "learning_rate=0.75", "max_features=9", "max_depth=3"])
     classifiers.append({'title': title, 'model': classifier})
     # model, y_pred, cm = run_classifier(classifier, X_train, X_test, y_train, y_test)
     # subtitle = subtitle_generator(y_test, y_pred)
     # plot_confusion_matrix(cm, show=False, save=True, filename='gradient_boosting', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
 
-    ## classifier 14 - Neural Network
+    # classifier 14 - Neural Network
 
-    classifier = MLPClassifier(hidden_layer_sizes=(8,8,8), activation='relu', solver='adam', max_iter=3000, random_state=2)
-    title = title_generator(["MLP", "hidden_layer_sizes=(8,8,8)", "activation='relu'", "solver='adam'", "max_iter=3000"])
+    classifier = MLPClassifier(hidden_layer_sizes=(
+        8, 8, 8), activation='relu', solver='adam', max_iter=3000, random_state=2)
+    title = title_generator(["MLP", "hidden_layer_sizes=(8,8,8)",
+                             "activation='relu'", "solver='adam'", "max_iter=3000"])
     classifiers.append({'title': title, 'model': classifier})
     # model, y_pred, cm = run_classifier(classifier, X_train, X_test, y_train, y_test)
     # subtitle = subtitle_generator(y_test, y_pred)
     # plot_confusion_matrix(cm, show=False, save=True, filename='mlp', title=title, subtitle=subtitle)
     # print(cross_validation(classifier, X_lda, y), title)
-    
-    ## classifier 15 - MLP tweaked
-    parameter_space = {
-        'hidden_layer_sizes': [(10,10,10), (20, 20, 20), (10, 20, 10)],
-        'activation': ['tanh', 'relu'],
-        'solver': ['sgd', 'adam'],
-        'alpha': [0.0001, 0.05],
-        'learning_rate': ['constant', 'adaptive'],
-    }
-    mlp = MLPClassifier(hidden_layer_sizes=(
-        8, 8, 8), activation='relu', solver='adam', max_iter=3000, random_state=2)
-    classifier = GridSearchCV(mlp, parameter_space, n_jobs=7, cv=3)
-    title = title_generator(["MLP", "tweaked parameters (with a grid search)"])
-    classifiers.append({'title': title, 'model': classifier})
-    
-    
+
+    # ## classifier 15 - MLP tweaked
+    # parameter_space = {
+    #     'hidden_layer_sizes': [(10,10,10), (20, 20, 20), (10, 20, 10)],
+    #     'activation': ['tanh', 'relu'],
+    #     'solver': ['sgd', 'adam'],
+    #     'alpha': [0.0001, 0.05],
+    #     'learning_rate': ['constant', 'adaptive'],
+    # }
+    # mlp = MLPClassifier(hidden_layer_sizes=(
+    #     8, 8, 8), activation='relu', solver='adam', max_iter=3000, random_state=2)
+    # classifier = GridSearchCV(mlp, parameter_space, n_jobs=7, cv=3)
+    # title = title_generator(["MLP", "tweaked parameters (with a grid search)"])
+    # classifiers.append({'title': title, 'model': classifier})
+
     for classifier in classifiers:
-        print(cross_validation(classifier['model'], X_lda, y), classifier['title'])
+        print(cross_validation(
+            classifier['model'], X_lda, y), classifier['title'])
 
     ################################
 
@@ -394,43 +420,44 @@ def main():
 
     classifier = SVC(kernel='rbf', gamma=0.0204, random_state=0, C=0.9)
     title = title_generator(["SVC", "kernel='rbf'", "gamma=0.0204", "C=0.9"])
-    model, y_pred, cm = run_classifier(classifier, X_train, X_test, y_train, y_test)
+    model, y_pred, cm = run_classifier(
+        classifier, X_train, X_test, y_train, y_test)
     subtitle = subtitle_generator(y_test, y_pred)
-    plot_confusion_matrix(cm, show=False, save=True, filename='svc_rbf_tweaked', title=title, subtitle=subtitle)
+    plot_confusion_matrix(cm, show=False, save=True,
+                          filename='svc_rbf_tweaked', title=title, subtitle=subtitle)
 
     # print(cross_validation(classifier, X_lda, y))
     # print(cross_validation(classifier, X_scaled, y))
 
-        # plot_confusion_matrix(cm, show=False, save=True, filename='svc_rbf_C'+str(C), title=title, subtitle=subtitle)
+    # plot_confusion_matrix(cm, show=False, save=True, filename='svc_rbf_C'+str(C), title=title, subtitle=subtitle)
 
-    # Tweaking the MLP parameters
-    parameter_space = {
-        'hidden_layer_sizes': [(50, 50, 50), (50, 100, 50), (100,)],
-        'activation': ['tanh', 'relu'],
-        'solver': ['sgd', 'adam'],
-        'alpha': [0.0001, 0.05],
-        'learning_rate': ['constant', 'adaptive'],
-    }
-    mlp = MLPClassifier(hidden_layer_sizes=(
-        8, 8, 8), activation='relu', solver='adam', max_iter=3000, random_state=2)
-    clf = GridSearchCV(mlp, parameter_space, n_jobs=7, cv=3)
-    clf.fit(X_lda, y)
+    # # Tweaking the MLP parameters
+    # parameter_space = {
+    #     'hidden_layer_sizes': [(50, 50, 50), (50, 100, 50), (100,)],
+    #     'activation': ['tanh', 'relu'],
+    #     'solver': ['sgd', 'adam'],
+    #     'alpha': [0.0001, 0.05],
+    #     'learning_rate': ['constant', 'adaptive'],
+    # }
+    # mlp = MLPClassifier(hidden_layer_sizes=(
+    #     8, 8, 8), activation='relu', solver='adam', max_iter=3000, random_state=2)
+    # clf = GridSearchCV(mlp, parameter_space, n_jobs=7, cv=3)
+    # clf.fit(X_lda, y)
 
     # Plotting the distributions
     for var_name in ['tempo', 'beats', 'chroma_stft', 'rmse',
-        'spectral_centroid', 'spectral_bandwidth', 'rolloff',
-        'zero_crossing_rate', 'mfcc1', 'mfcc2', 'mfcc3', 'mfcc4', 'mfcc5',
-        'mfcc6', 'mfcc7', 'mfcc8', 'mfcc9', 'mfcc10', 'mfcc11', 'mfcc12',
-        'mfcc13', 'mfcc14', 'mfcc15', 'mfcc16', 'mfcc17', 'mfcc18', 'mfcc19',
-        'mfcc20']:
+                     'spectral_centroid', 'spectral_bandwidth', 'rolloff',
+                     'zero_crossing_rate', 'mfcc1', 'mfcc2', 'mfcc3', 'mfcc4', 'mfcc5',
+                     'mfcc6', 'mfcc7', 'mfcc8', 'mfcc9', 'mfcc10', 'mfcc11', 'mfcc12',
+                     'mfcc13', 'mfcc14', 'mfcc15', 'mfcc16', 'mfcc17', 'mfcc18', 'mfcc19',
+                     'mfcc20']:
         distribution_plot(data[var_name], var_name)
 
+    # distance matrix
 
-    ###### distance matrix
-    
     # dist_mat = squareform(pdist(data))
     # print(dist_mat)
-        
+
     # N = len(data)
     # plt.pcolormesh(dist_mat)
     # plt.colorbar()
@@ -444,7 +471,7 @@ def main():
     # print(data.describe())
     # print(data.corr())
 
-    ## PCA
+    # PCA
     # projected = pca(x,y,2)
     # label = [x//100 for x in range(1000)]
     # colors = ['red','green','blue','purple','gray','brown','orange','cyan','pink','olive']
@@ -458,6 +485,7 @@ def main():
     # plt.xlabel('component 1')
     # plt.ylabel('component 2')
     # plt.show()
+
 
 if __name__ == "__main__":
     main()
